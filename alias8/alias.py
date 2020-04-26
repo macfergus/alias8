ada0bceab21680d068b48a69aa88a0e1a69cac70
Kevin Ferguson <kevin.a.ferguson@gmail.com>
2020-04-25 17:08:06 -0700

    Switch top button to mute.
    Was solo

--- alias8/alias.py
+++ alias8/alias.py
@@ -3,7 +3,14 @@
 
 from __future__ import with_statement
 
-DEBUG = False
+g_logger = None
+DEBUG = True
+
+def log(msg):
+    global g_logger
+    if DEBUG:
+        if g_logger is not None:
+            g_logger(msg)
 
 import Live
 import MidiRemoteScript
@@ -17,6 +24,15 @@ from _Framework.SessionComponent import SessionComponent
 from _Framework.SliderElement import SliderElement
 
 CHANNEL = 0
+
+# Channel strip mapping:
+# knobs -> first 2 controls on first device on the channel
+# fader -> channel volume
+# top button -> channel mute
+# bottom button -> channel arm
+
+# master fader -> master volume
+# encoder -> move grid
 
 # Alias 8 color map.
 WHITE = 2
@@ -38,7 +54,7 @@ def button(notenr, name=None, color=None):
 
 def fader(notenr):
     rv = SliderElement(MIDI_CC_TYPE, CHANNEL, notenr)
-    return rv 
+    return rv
 
 def knob(cc):
     return EncoderElement(MIDI_CC_TYPE, 0, cc, Live.MidiMap.MapMode.absolute)
@@ -51,6 +67,7 @@ class ColorButton(ButtonElement):
         self.button_value = color
 
     def turn_on(self):
+        log('ColorButton turn on ' + str(self.button_value))
         self.send_value(self.button_value)
 
 class Alias8(ControlSurface):
@@ -62,14 +79,16 @@ class Alias8(ControlSurface):
     encoder = 42
     buttons_top = [0, 1, 2, 3, 4, 5, 6, 7]
     buttons_bottom = [8, 9, 10, 11, 12, 13, 14, 15]
-    
+
     def __init__(self, instance):
         super(Alias8, self).__init__(instance, False)
+        global g_logger
+        g_logger = self.log_message
         with self.component_guard():
             self._set_suppress_rebuild_requests(True)
             self.init_session()
-            self.init_mixer() 
-            
+            self.init_mixer()
+
             # Connect mixer to session.
             self.session.set_mixer(self.mixer)
             self.session.update()
@@ -94,13 +113,14 @@ class Alias8(ControlSurface):
 
     def init_mixer(self):
         self.mixer = MixerComponent(self.num_tracks, 0)
-        self.mixer.id = 'Mixer' 
+        self.mixer.id = 'Mixer'
         self.song().view.selected_track = self.mixer.channel_strip(0)._track
         for i in range(self.num_tracks):
             self.mixer.channel_strip(i).set_volume_control(
                     fader(self.faders[i]))
-            self.mixer.channel_strip(i).set_solo_button(
-                    button(self.buttons_top[i], color=CYAN))
+            self.mixer.channel_strip(i).set_mute_button(
+                    button(self.buttons_top[i], color=YELLOW))
+            self.mixer.channel_strip(i).set_invert_mute_feedback(True)
             self.mixer.channel_strip(i).set_arm_button(
                     button(self.buttons_bottom[i], color=RED))
             self.mixer.channel_strip(i).set_pan_control(
